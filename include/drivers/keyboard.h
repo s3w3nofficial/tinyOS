942b1a098900ded9992a76245688cd901e440449
s3w3nofficial <s3w3nofficial@gmail.com>
2022-12-25 19:49:20 +0100

    clean up

--- include/drivers/keyboard.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stddef.h>
-
-char get_input_keycode();
-void sleep(uint32_t timer_count);
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,35 +1,13 @@
-//#include "char.h"
-//#include "keyboard.h"
 #include "print.h"
 #include "gdt.h"
 #include <stdint.h>
-
-/*
-void test_input()
-{
-  char ch = 0;
-  char keycode = 0;
-  do{
-    keycode = get_input_keycode();
-    if(keycode == KEY_ENTER){
-      print_newline();
-    }else{
-      ch = get_ascii_char(keycode);
-      print_char(ch);
-    }
-    sleep(0x02FFFFFF);
-  }while(ch > 0);
-}
-*/
 
 void kernel_main() {
   print_clear();
   print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
   print_str("Welcome to our 64-bit kernel!");
 
-  //print_newline();
-
-  //test_input();
+  print_newline();
 
   GlobalDescriptorTable * gdt = gdt_init();
 }
